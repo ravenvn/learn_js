f7f0495185b29c4779ebe8b6b1b130520845acc8
Vinh Ngo <nanyangbk@gmail.com>
2021-07-23 21:51:44 +0700

    init test 1 .js

--- test1.js
+++ test1.js
@@ -1,1 +1,54 @@
-console.log("Hello");
+// biến
+// variable
+var name = "Thịnh";
+
+// Ctrl + / 
+// console.log(name);
+
+name = "Nam " + "Thịnh";
+// console.log(name);
+
+var age = 18;
+
+age = age - 5;
+
+// console.log(age);
+// constant
+
+const MAX_AGE = 200;
+// MAX_AGE = 300;
+// console.log(MAX_AGE);
+
+// toán tử operation.
+console.log(4 % 5);
+
+// gán giá trị.
+var age = 18;
+
+// kiểu dữ liệu.
+// số nguyên: 1,2,3,5,-1,-4
+// Number => 4 bytes.
+// 1 bit. số 0 số 1.
+// đúng / sai. 1 bit. đúng => 1, sai = 0.
+//  1 byte = 8 bit. 
+// số nguyên Number. 8 bytes => 64 bit. 
+// 
+var isHandsome = false;
+// String : chuỗi. 
+// tên người: Nguyễn văn,
+// Số 10 kom tum. chuỗi.
+// Object => vật thể, đối tượng 
+var person {
+  height: 150,
+  weight: 40,
+  grade: 7,
+  name: "Nam Thinh",
+  color: "Red"
+};
+
+
+
+
+
+
+
